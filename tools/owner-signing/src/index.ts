885bfcb7e30a544f4dd3f566ec7db3624da4425c
Yastroyka <mhitryakov@gmail.com>
2026-08-28 02:05:21 +0300

    TASK-018: export owner signing API

---
+++ tools/owner-signing/src/index.ts
@@ -0,0 +1,7 @@
+export {
+  parseVkCommunityApprovalPacketOutput,
+  runVkCommunityOwnerGrantCli,
+  type VkCommunityOwnerGrantCliDependencies,
+  type VkCommunityOwnerGrantCliExitCode,
+  type VkCommunityOwnerGrantCliIo,
+} from './vk-community-owner-grant-cli.ts';
